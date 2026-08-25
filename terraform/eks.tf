e2408ab5cf030ae89213b1adaa77e33206d881f7
JoaoFelipe4 <johaoflp93@gmail.com>
2026-08-24 21:51:35 -0300

    feat(eks): add dynamic IAM access entry for admin user

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -13,6 +13,11 @@ resource "aws_eks_cluster" "main" {
     endpoint_public_access = true
   }
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy
   ]
@@ -44,4 +49,21 @@ resource "aws_eks_node_group" "main" {
     aws_iam_role_policy_attachment.eks_cni_policy,
     aws_iam_role_policy_attachment.eks_ecr_readonly,
   ]
+}
+
+# Permissao de Administrador no Cluster para o usuario especifico via variavel
+resource "aws_eks_access_entry" "admin_user" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = var.admin_user_arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admin_user_policy" {
+  cluster_name  = aws_eks_cluster.main.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = var.admin_user_arn
+
+  access_scope {
+    type = "cluster"
+  }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "admin_user_arn" {
+  type        = string
+  description = "ARN do usuario IAM que tera acesso de administrador no cluster EKS"
+}
