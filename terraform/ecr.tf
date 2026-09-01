9f3627618785f45022a1470b75a8d9cbf1fb40cb
JoaoFelipe4 <johaoflp93@gmail.com>
2026-09-01 14:29:04 -0300

    fix(infra): use correct force_delete argument for ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "backend" {
   name                 = "k8s-project-backend"
   image_tag_mutability = "MUTABLE"
-  force_destroy        = true
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
